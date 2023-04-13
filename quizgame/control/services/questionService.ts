507413405f35fb1bd794653d4cbf16ffa1354aeb
Gabriel Lima <gabriellimab@gmail.com>
2023-04-13 20:40:59 -0300

    feat: cria serviço de chamada a API

---
+++ quizgame/control/services/questionService.ts
@@ -0,0 +1,27 @@
+const API_KEY = 'zHrUsAYXvCDsygrhtoBqRoBAvbgHm9UvpLJ87w1c';
+const API_URL = 'https://quizapi.io/api/v1/questions?apiKey=${API_KEY}';
+
+function getQuestions(
+  qtdQuestions: number,
+  difficulty?: string,
+  category?: string
+): Promise<Question[]> {
+  // For now, consider the data is stored on a static `users.json` file
+  let FINAL_URL = `${API_URL}&limit=${qtdQuestions}`;
+  if (difficulty) {
+    FINAL_URL.concat(`&difficulty=${difficulty}`);
+  }
+  if (category) {
+    FINAL_URL.concat(`&category=${category}`);
+  }
+  return (
+    fetch(FINAL_URL)
+      // the JSON body is taken from the response
+      .then((res) => res.json())
+      .then((res) => {
+        // The response has an `any` type, so we need to cast
+        // it to the `User` type, and return it from the promise
+        return res as Question[];
+      })
+  );
+}
