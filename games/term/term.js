d1d70858de7a5c0077821b77fae982de2d81c1da
Daniel Caumo <dancaumo@gmail.com>
2023-04-24 20:22:09 -0300

    new-corrections

--- games/term/term.js
+++ games/term/term.js
@@ -155,7 +155,7 @@ createTileBoard(size);
 const keys = [
     'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
     'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '«',
-    'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'enter'
+    'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'enter',
 ]
 
 keys.forEach(key => {
@@ -256,7 +256,7 @@ const showMessage = (message, duration = 0, callback) => {
       setTimeout(() => {
         messageElement.textContent = '';
         messageElement.style.display = 'none';
-
+        callback();
       }, duration);
     }
 };
